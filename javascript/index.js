78be42f1f339328d1cbd87648420c4fb71f1e610
caidle <67764059+caidle@users.noreply.github.com>
2024-03-02 15:43:45 -0500

    Change city with dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -20,3 +20,23 @@ function UpdateTime() {
 
 UpdateTime();
 setInterval(UpdateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(`${cityTimeZone}`);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small]"
+          )}</div>
+        </div>`;
+  setInterval(updateCity, 1000);
+}
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
